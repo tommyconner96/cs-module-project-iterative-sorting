8df85194816d5d5ea2547b24f71ad93345bf6c77
unknown <tommyconner96@gmail.com>
2020-09-02 20:48:56 -0400

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -14,7 +14,25 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
 
+    # start = the starting index of the subset of the array being searched
+    start = 0
+    # end = the end index of the subset
+    end = len(arr) -1 
+
+    while end >= start:
+        # get the modpoint index and value
+        middle_index = (start + end) // 2
+        middle_value = arr[middle_index]
+        # compare it to the target
+        if target == middle_value:
+            return middle_index
+
+        if target > middle_value:
+            # search the right side
+            # set start = middle_index + 1
+            start = middle_index + 1
+        if target < middle_value:
+            end = middle_index - 1
 
     return -1  # not found
