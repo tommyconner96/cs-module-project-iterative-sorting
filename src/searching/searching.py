1f29e8de754d8a0bcadd74b75a7ca6999b8c590c
unknown <tommyconner96@gmail.com>
2020-09-02 20:32:25 -0400

    linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,13 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    # linear search function will return the location in the arr of
+    # the value searcehd, if it exists
+    # loop thru the arr
+    for i in range(len(arr)):
+        # compare target
+        if arr[i] == target:
+            # if equal
+            return i
+        # else keep looping
     return -1   # not found
 
 
